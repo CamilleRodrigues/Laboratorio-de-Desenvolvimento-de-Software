5dd9a7ca836083d753f3f5ead0ee445d26bdb196
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-07-29 21:10:50 -0300

    Clean up FormaGeometrica.java comments and package
    Removed comments and updated package declaration.

---
+++ Exercicio3/FormaGeometrica.java
@@ -0,0 +1,10 @@
+package ExercicioDois;
+
+public class FormaGeometrica {
+    public float lado;
+    public float area;
+    
+    public void calcularArea() {
+        area = lado * lado;
+    }
+}
