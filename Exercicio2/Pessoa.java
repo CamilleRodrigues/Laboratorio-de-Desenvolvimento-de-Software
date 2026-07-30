5324f904002435cff36e44035b09cba190e58999
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-07-29 21:07:49 -0300

    Add Pessoa class with name and CPF attributes

---
+++ Exercicio2/Pessoa.java
@@ -0,0 +1,28 @@
+package Heranca;
+
+public class Pessoa {
+    protected String nome;
+    protected String cpf;
+   
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getCpf() {
+        return nome;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{ " + "nome: " + nome + ", cpf: " + cpf + '}';
+    }
+    
+}
