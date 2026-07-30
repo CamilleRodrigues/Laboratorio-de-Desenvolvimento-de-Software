603a3a2e594aa10e23ce397fab40751a3c675f70
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-07-29 21:11:45 -0300

    Add Principal class to calculate house price

---
+++ Exercicio4/Principal.java
@@ -0,0 +1,9 @@
+package ExercicioTres;
+
+public class Principal {
+    public static void main(String[] args) {
+        Casa c = new Casa();
+        float valor = c.calcularPreco(52);
+        System.out.println("Valor da casa: " + valor);
+    }
+}
