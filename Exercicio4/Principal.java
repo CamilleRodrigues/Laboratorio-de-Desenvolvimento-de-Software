7b5ebcfce810a9d29bd37bb805279844e6ee9adc
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-07-29 21:14:15 -0300

    Add additional house price calculations and outputs

--- Exercicio4/Principal.java
+++ Exercicio4/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
     public static void main(String[] args) {
         Casa c = new Casa();
         float valor = c.calcularPreco(52);
-        System.out.println("Valor da casa: " + valor);
+        System.out.println("Valor da casa 1: " + valor);
+        valor = c.calcularPreco(52, 3);
+        System.out.println("Valor da casa 2: " + valor);
     }
 }
