66bb1b219e54cd4249400980359d33fd17cba913
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-09-23 20:56:13 -0300

    Create Pessoa.java

---
+++ Aula 2309/Pessoa.java
@@ -0,0 +1,42 @@
+package beans;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String sexo;
+    private String Idioma;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getIdioma() {
+        return Idioma;
+    }
+
+    public void setIdioma(String Idioma) {
+        this.Idioma = Idioma;
+    }
+    
+    
+}
