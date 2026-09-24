6b16fe9fe9319fe7998258e78bc88f03292c21b7
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-09-23 21:05:37 -0300

    Update ProjetoAulaBD.java

--- Aula 2309/ProjetoAulaBD.java
+++ Aula 2309/ProjetoAulaBD.java
@@ -1,11 +1,18 @@
 package projetoaulabd;
-import projetoaulabd.conexao.conexao;
+import beans.Pessoa;
+import dao.PessoaDao;
+import projetoaulabd.conexao.Conexao;
 
 public class ProjetoAulaBD {
 
     public static void main(String[] args) {
-        conexao c = new conexao();
+        Conexao c = new Conexao();
         c.getConexao();
+        Pessoa p = new Pessoa();
+        p.setNome("Camille Rodrigues");
+        p.setSexo("F");
+        p.setIdioma("Português");
+        PessoaDao pdao = new PessoaDao();
+        pdao.inserir(p);
     }
-    
 }
